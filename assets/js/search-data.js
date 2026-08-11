45e1dbe394f41a738cd79c5d2625194dbb751e47
cathyshyr <31411273+cathyshyr@users.noreply.github.com>
2026-08-11 17:10:29 +0000

    Deploying to gh-pages from @ cathyshyr/cathyshyr.github.io@7d74cfaecf77aae0c7fc0bc0858720748d1ffdce 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,6 +23,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-software",
+          title: "Software",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/software/";
+          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
